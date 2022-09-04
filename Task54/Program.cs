752bc69c59f1de790989b1af8e9e75bcd63de617
Elena Sinyagina <lenaakimova08@rambler.ru>
2022-09-04 15:19:27 +0400

    Task 54

---
+++ Task54/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного
+// массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    var result = new int[m, n];
+
+    var random = new Random();
+
+    for (var i = 0; i < result.GetLength(0); i++)
+    {
+        for (var j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = random.Next(0, 11);
+        }
+    }
+
+    return result;
+}
+
+int[,] ArrayWithSortingString(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++) {
+        for (var j = 0; j < array.GetLength(1); j++) {
+            for (var k = 0; k < array.GetLength(1) - j - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int l = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = l;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+Console.WriteLine("Введите число строк (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m))
+{
+    Console.WriteLine("Всё плохо");
+}
+
+Console.WriteLine("Введите число столбцов (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n))
+{
+    Console.WriteLine("Всё плохо");
+}
+
+var array = CreateArrayWithRandomNumbers(m, n);
+PrintArray(array);
+Console.WriteLine();
+var result = ArrayWithSortingString(array);
+Console.WriteLine();
+PrintArray(result);
