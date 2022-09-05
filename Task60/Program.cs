11e5885c20e58528459219880bb5bb26e85dc357
Elena Sinyagina <lenaakimova08@rambler.ru>
2022-09-05 21:55:52 +0400

    Task 60

---
+++ Task60/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся
+// двузначных чисел. Напишите программу, которая будет построчно
+// выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int x = 2;
+int y = 2;
+int z = 2;
+
+int[,,] array3D = new int[x, y, z];
+CreateArray(array3D);
+Console.WriteLine("Выведем массив и индексы каждому элементу:");
+PrintArrayIndex(array3D);
+
+void CreateArray(int[,,] array3D)
+{
+    int[] array = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+    int number;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        array[i] = new Random().Next(10, 20);
+        number = array[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (array[i] == array[j])
+                {
+                    array[i] = new Random().Next(10, 20);
+                    j = 0;
+                    number = array[i];
+                }
+                number = array[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array3D.GetLength(0); x++)
+    {
+        for (int y = 0; y < array3D.GetLength(1); y++)
+        {
+            for (int z = 0; z < array3D.GetLength(2); z++)
+            {
+                array3D[x, y, z] = array[count];
+                count++;
+            }
+        }
+    }
+}
+void PrintArrayIndex(int[,,] array3D)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.Write($"");
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]} ({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
